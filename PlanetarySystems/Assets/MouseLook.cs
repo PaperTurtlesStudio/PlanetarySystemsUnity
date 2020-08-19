a361a821fdf643575dd073187fe9e365bfae5857
PaperTurtlesStudio <66807368+PaperTurtlesStudio@users.noreply.github.com>
2020-08-18 23:24:25 -0700

    PlanetBlocking/Movement

--- PlanetarySystems/Assets/MouseLook.cs
+++ PlanetarySystems/Assets/MouseLook.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+	public float mouseSensitivity = 100f;
+	public Transform playerBody;
+	
+	private float xRotation = 0f;
 
-    public float MouseSensitivity = 100.0f;
-    public Transform PlayerBody;
-    float xRotation = 0.0f;
+	// Start is called before the first frame update
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
+	// Update is called once per frame
+	void Update()
+	{
+			Cursor.visible = false;
 
+			float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+			xRotation -= mouseY;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+			xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        if (mouseY < 0.1 && mouseY > -0.1)
-        {
-            mouseY = 0;
-        }
-        if (mouseX < 0.1 && mouseX > -0.1)
-        {
-            mouseX = 0;
-        }
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
-        PlayerBody.Rotate(Vector3.up * mouseX);
-    }
+			transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+			playerBody.Rotate(Vector3.up * mouseX);
+	}
 }
