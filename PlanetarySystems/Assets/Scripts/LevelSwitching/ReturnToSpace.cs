314b0898dff40280d07df6a35bbbff614a4432a5
PaperTurtlesStudio <66807368+PaperTurtlesStudio@users.noreply.github.com>
2020-08-20 13:49:42 -0700

    Renaming

---
+++ PlanetarySystems/Assets/Scripts/LevelSwitching/ReturnToSpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToSpace : MonoBehaviour
+{
+    public GameObject Spaceship;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(Spaceship.transform.position, transform.position) < 20.0f && Input.GetKey(KeyCode.L))
+        {
+            Debug.Log("Changing Scenes");
+
+            SceneManager.LoadScene("SpaceBlocking");
+        }
+        else
+        {
+            //Tell not close enough to ship to return to space
+        }
+    }
+}
